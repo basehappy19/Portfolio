05b8c25d826d3701a83698205b74a629940e1579
basehappy19 <pakpoom.td@gmail.com>
2025-12-02 00:01:36 +0700

    Remove Config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-    images: {
-        domains: ["via.placeholder.com"],
-    },
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
