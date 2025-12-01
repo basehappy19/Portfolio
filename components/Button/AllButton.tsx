89b587a5136d217e014586f5c29110cf1a8138eb
basehappy19 <pakpoom.td@gmail.com>
2025-12-02 00:29:30 +0700

    Add Handle

--- components/Button/AllButton.tsx
+++ components/Button/AllButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { CurrentCategory } from "../CategoryFilterTab";
 
 type AllButtonProps = {
@@ -9,13 +10,22 @@ type AllButtonProps = {
 };
 
 const AllButton = ({ TotalAchievements, AllButtonLabel, CurrentCategory }: AllButtonProps) => {
-    const isActive = !CurrentCategory; 
+    const isActive = !CurrentCategory;
+    const pathname = usePathname();
+    const router = useRouter();
+    const searchParams = useSearchParams();
 
+    const handleClick = () => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.delete("category");
+        router.push(`${pathname}`, { scroll: false });
+    };
     return (
         <button
+            onClick={handleClick}
             className={`cursor-pointer px-6 py-2.5 rounded-full font-medium transition-all ${isActive
-                    ? 'bg-red-600 text-white shadow-lg shadow-red-500/30'
-                    : 'bg-gray-100 text-gray-700 hover:bg-gray-200 hover:shadow-md'
+                ? 'bg-red-600 text-white shadow-lg shadow-red-500/30'
+                : 'bg-gray-100 text-gray-700 hover:bg-gray-200 hover:shadow-md'
                 }`}
         >
             {AllButtonLabel} ({TotalAchievements})
