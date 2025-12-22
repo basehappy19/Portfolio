b4d1674a3ced3565f77dd931892c0fad012d3f4f
Notebook <pakpoom.td@gmail.com>
2025-12-22 13:59:53 +0700

    Edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "react-hot-toast";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
 import NextTopLoader from 'nextjs-toploader';
 
 export default async function RootLayout({
@@ -13,8 +11,6 @@ export default async function RootLayout({
     return (
         <html suppressHydrationWarning>
             <body>
-                <SpeedInsights />
-                <Analytics />
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
